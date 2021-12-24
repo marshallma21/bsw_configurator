f9045e76cf8fc1b807caf6c98e1161edbadc5fa0
Marshall Ma <marshall.ma@aliyun.com>
2021-12-25 01:29:10 +0800

    Improve generation pipline and xml apis

--- code_gen.py
+++ code_gen.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, FileSystemLoader
+from xml_parse import *
 
 ChannelGroupIndex = 0
 
@@ -13,82 +14,7 @@ DioInfo = {
     'InOutConfigSupported': False
 }
 
-Dio = {
-    "CommonPublishedInformation": {
-        "ArReleaseMajorVersion": 4,
-        "ArReleaseMinorVersion": 2,
-        "ArReleaseRevisionVersion": 2,
-        "ModuleId": 120,
-        "SwMajorVersion": 1,
-        "SwMinorVersion": 0,
-        "SwPatchVersion": 2,
-        "VendorId": 43
-    },
-    "DioConfig": [
-        {
-            "DioChannel": [
-                {
-                    "DioChannelId": 13,
-                    "name": "Dio_SWT_CCW"
-                },
-                {
-                    "DioChannelId": 12,
-                    "name": "Dio_SWT_CW"
-                }
-            ],
-            "DioChannelGroup": [
-                {
-                    "DioChannelGroupIdentification": "DioChannelGroup",
-                    "DioPortBitNumber": 1,
-                    "DioPortMask": 1,
-                    "DioPortOffset": 0,
-                    "name": "DioChannelGroup_0"
-                }
-            ],
-            "DioPortId": 2,
-            "name": "DioPort_PortC"
-        },
-        {
-            "DioChannel": [
-                {
-                    "DioChannelId": 0,
-                    "name": "Dio_RelayA"
-                },
-                {
-                    "DioChannelId": 15,
-                    "name": "Dio_RelayB"
-                }
-            ],
-            "DioChannelGroup": [
-                {
-                    "DioChannelGroupIdentification": "DioChannelGroup2",
-                    "DioPortBitNumber": 1,
-                    "DioPortMask": 1,
-                    "DioPortOffset": 0,
-                    "name": "DioChannelGroup_1"
-                },
-                {
-                    "DioChannelGroupIdentification": "DioChannelGroup3",
-                    "DioPortBitNumber": 1,
-                    "DioPortMask": 1,
-                    "DioPortOffset": 0,
-                    "name": "DioChannelGroup_2"
-                }
-            ],
-            "DioPortId": 3,
-            "name": "DioPort_PortD"
-        }
-    ],
-    "DioGeneral": {
-        "DioDevErrorDetect": 0,
-        "DioEnableUserModeSupport": 0,
-        "DioFlipChannelApi": 0,
-        "DioMaskedWritePortApi": 0,
-        "DioReadZeroForUndefinedPortPins": 0,
-        "DioReversePortBits": 0,
-        "DioVersionInfoApi": 0
-    }
-}
+Dio = load_DIO_config('arxml/MCAL_config.arxml')
 
 
 def inttohex(num, zfillnum=0):
@@ -130,15 +56,16 @@ env.lstrip_blocks = True
 env.rstrip_blocks = True
 env.filters['inttohex'] = inttohex
 
-file_name = 'Dio_Cfg.h'
-template = env.get_template(file_name)
-template.globals['inttohex'] = inttohex
-template.globals['get_ChannelGroupIndex'] = get_ChannelGroupIndex
-template.globals['get_numChannelGroupsInConfig'] = get_numChannelGroupsInConfig
-template.globals['get_mask_offset'] = get_mask_offset
 
+file_names = ['Dio_Cfg.h', 'Dio_Cfg.c']
+for file_name in file_names:
+    ChannelGroupIndex = 0
+    template = env.get_template(file_name)
+    template.globals['inttohex'] = inttohex
+    template.globals['get_ChannelGroupIndex'] = get_ChannelGroupIndex
+    template.globals['get_numChannelGroupsInConfig'] = get_numChannelGroupsInConfig
+    template.globals['get_mask_offset'] = get_mask_offset
 
-
-
-with open('generate/%s'%file_name, 'w') as f:
-    f.write(template.render(DioInfo=DioInfo, Dio=Dio))
+    with open('generate/%s'%file_name, 'w') as f:
+        f.write(template.render(DioInfo=DioInfo, Dio=Dio))
+        print('[OK] Generate %s'%file_name)
--- xml_parse.py
+++ xml_parse.py
@@ -6,6 +6,8 @@ xmlschema.limits.MAX_MODEL_DEPTH = 100
 xsd_path = 'AUTOSAR_MMOD_XMLSchema/AUTOSAR_4-2-2.xsd'
 my_schema = xmlschema.XMLSchema(xsd_path)
 
+root = None
+nsmap = None
 
 def validate_arxml(arxml_file):
     try:
@@ -25,88 +27,87 @@ def get_target_ARPackage(mod_name):
     return None
 
 def convert_para_type(data, para_type):
-    if para_type == 'ECUC-NUMERICAL-PARAM-VALUE':
+    if para_type == 'ECUC-TEXTUAL-PARAM-VALUE':
+        return data
+    elif para_type == 'ECUC-NUMERICAL-PARAM-VALUE':
         return int(data)
     else:
         print('data type [%s] not supported'%para_type)
         return data
 
-def sub_container_recurrsive():
-    pass
+def load_DIO_config(arxml_file):
+    global root, nsmap
+    if validate_arxml(arxml_file):
+        print('[OK] Validation pass: %s'%arxml_file)
+    else:
+        return
 
-arxml_file = 'arxml/MCAL_config.arxml'
+    tree = ET.parse(arxml_file)
+    root = tree.getroot()
+    nsmap = root.nsmap
 
-if validate_arxml(arxml_file):
-    print('[OK] Validation pass: %s'%arxml_file)
-else:
-    exit()
+    mod_pkg = get_target_ARPackage('Dio')
+    DioInfo = {}
+    Dio = {}
+
+    if mod_pkg != None:
+        elements = mod_pkg.find('.//ELEMENTS/', nsmap)
+        for el in elements:
+            if len(el) == 0:
+                DioInfo[el.tag[el.tag.rfind('}')+1:]] = el.text
+
+        containers = mod_pkg.findall('.//CONTAINERS/ECUC-CONTAINER-VALUE', nsmap)
+        for container in containers:
+            cont_name = container.find('./SHORT-NAME', nsmap).text
+
+            if cont_name == 'DioGeneral':
+                paras = container.findall('./PARAMETER-VALUES/', nsmap)
+                if len(paras) > 0:
+                    Dio[cont_name] = {}
+                    for para in paras:
+                        para_type = para.tag[para.tag.rfind('}')+1:]
+                        key, value = para.findall('./')
+                        Dio[cont_name][key.text[key.text.rfind('/')+1:]] = convert_para_type(value.text, para_type)
+            elif cont_name == 'DioConfig':
+                DioPorts = container.findall('./SUB-CONTAINERS/ECUC-CONTAINER-VALUE', nsmap)
+                Dio[cont_name] = []
+                for DioPort in DioPorts:
+                    PortDict = {}
+                    PortDict['name'] = DioPort.find('./SHORT-NAME', nsmap).text
+                    PortDict['DioPortId'] = int(DioPort.find('./PARAMETER-VALUES/ECUC-NUMERICAL-PARAM-VALUE/VALUE', nsmap).text)
+                    
+                    DioChannels = DioPort.find('./SUB-CONTAINERS', nsmap)
+                    PortDict['DioChannel'] = []
+                    PortDict['DioChannelGroup'] = []
+
+                    for DioChannel in DioChannels:
+                        DefRef = DioChannel.find('./DEFINITION-REF', nsmap).text.split('/')
+                        ChannelDict = {}
+                        ChannelDict['name'] = DioChannel.find('./SHORT-NAME', nsmap).text
+                        ChannelParas = DioChannel.find('./PARAMETER-VALUES', nsmap)
+                        for ChannelPara in ChannelParas:
+                            ParaType = ChannelPara.tag[ChannelPara.tag.find('}')+1:]
+                            ParaName = ChannelPara.find('./DEFINITION-REF', nsmap).text.split('/')[-1]
+                            ParaValue = ChannelPara.find('./VALUE', nsmap).text
+                            ChannelDict[ParaName] = convert_para_type(ParaValue, ParaType)
+
+                        PortDict[DefRef[-1]].append(ChannelDict)
+                    Dio[cont_name].append(PortDict)
+    
+            else:
+                paras = container.findall('./PARAMETER-VALUES/', nsmap)
+                if len(paras) > 0:
+                    Dio[cont_name] = {}
+                    for para in paras:
+                        para_type = para.tag[para.tag.rfind('}')+1:]
+                        key, value = para.findall('./')
+                        Dio[cont_name][key.text[key.text.rfind('/')+1:]] = convert_para_type(value.text, para_type)
+    
+    #print(json.dumps(DioInfo, indent=4, sort_keys=True) )
+    #print(json.dumps(Dio, indent=4, sort_keys=True) )
+    return Dio
 
 
-tree = ET.parse(arxml_file)
-root = tree.getroot()
-nsmap = root.nsmap
-
-mod_pkg = get_target_ARPackage('Dio')
-DioInfo = {}
-Dio = {}
-
-if mod_pkg != None:
-    elements = mod_pkg.find('.//ELEMENTS/', nsmap)
-    for el in elements:
-        if len(el) == 0:
-            DioInfo[el.tag[el.tag.rfind('}')+1:]] = el.text
-
-    containers = mod_pkg.findall('.//CONTAINERS/ECUC-CONTAINER-VALUE', nsmap)
-    for container in containers:
-        cont_name = container.find('./SHORT-NAME', nsmap).text
-
-        if cont_name == 'DioGeneral':
-            paras = container.findall('./PARAMETER-VALUES/', nsmap)
-            if len(paras) > 0:
-                Dio[cont_name] = {}
-                for para in paras:
-                    para_type = para.tag[para.tag.rfind('}')+1:]
-                    key, value = para.findall('./')
-                    Dio[cont_name][key.text[key.text.rfind('/')+1:]] = convert_para_type(value.text, para_type)
-        elif cont_name == 'DioConfig':
-            DioPorts = container.findall('./SUB-CONTAINERS/ECUC-CONTAINER-VALUE', nsmap)
-            Dio[cont_name] = []
-            for DioPort in DioPorts:
-                PortDict = {}
-                PortDict['name'] = DioPort.find('./SHORT-NAME', nsmap).text
-                PortDict['DioPortId'] = int(DioPort.find('./PARAMETER-VALUES/ECUC-NUMERICAL-PARAM-VALUE/VALUE', nsmap).text)
-                
-                DioChannels = DioPort.find('./SUB-CONTAINERS', nsmap)
-                PortDict['DioChannel'] = []
-                PortDict['DioChannelGroup'] = []
-
-                for DioChannel in DioChannels:
-                    DefRef = DioChannel.find('./DEFINITION-REF', nsmap).text.split('/')
-                    ChannelDict = {}
-                    ChannelDict['name'] = DioChannel.find('./SHORT-NAME', nsmap).text
-                    ChannelParas = DioChannel.find('./PARAMETER-VALUES', nsmap)
-                    for ChannelPara in ChannelParas:
-                        ParaType = ChannelPara.tag[ChannelPara.tag.find('}')+1:]
-                        ParaName = ChannelPara.find('./DEFINITION-REF', nsmap).text.split('/')[-1]
-                        ParaValue = ChannelPara.find('./VALUE', nsmap).text
-                        if ParaType == 'ECUC-TEXTUAL-PARAM-VALUE':
-                            ChannelDict[ParaName] = ParaValue
-                        elif ParaType == 'ECUC-NUMERICAL-PARAM-VALUE':
-                            ChannelDict[ParaName] = int(ParaValue)
-
-                    PortDict[DefRef[-1]].append(ChannelDict)
-                Dio[cont_name].append(PortDict)
- 
-        else:
-            paras = container.findall('./PARAMETER-VALUES/', nsmap)
-            if len(paras) > 0:
-                Dio[cont_name] = {}
-                for para in paras:
-                    para_type = para.tag[para.tag.rfind('}')+1:]
-                    key, value = para.findall('./')
-                    Dio[cont_name][key.text[key.text.rfind('/')+1:]] = convert_para_type(value.text, para_type)
 
         
 
-#print(json.dumps(DioInfo, indent=4, sort_keys=True) )
-print(json.dumps(Dio, indent=4, sort_keys=True) )
